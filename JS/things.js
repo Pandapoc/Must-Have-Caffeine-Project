d1c2557f888e0d4a3b49022e953acbaba241aeb4
SPFlores <sflores92886@gmail.com>
2019-03-30 10:59:47 -0700

    JS fetch

--- JS/things.js
+++ JS/things.js
@@ -2,10 +2,8 @@ let apiUSDA = 'IIT5HHAkRjEtkiOxRLwoPmagdVBOWMfmfba7JXHu'
 
 let searchItem = 'coffee'
 
-fetch(`https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=n&max=25&offset=0&api_key=${apiUSDA}`)
+fetch(`https://api.nal.usda.gov/ndb/search/?format=json&q=butter&sort=r&max=25&offset=0&api_key=${apiUSDA}`)
   .then(r => r.json())
   .then(r => {
-    console.log(r.list.item)
+    console.log(r)
   })
-
-  stuff and things
