64f4f17a3efa9fe0abf00fa8a43dc571c1d06d71
SPFlores <sflores92886@gmail.com>
2019-04-04 20:37:10 -0700

    ingredient info table

--- JS/usdaAPI.js
+++ JS/usdaAPI.js
@@ -9,6 +9,12 @@ let database
 let offset = 0
 let ingredientInfo = {}
 
+document.addEventListener('click', e => {
+  if (e.target.className === 'ingredientOption') {
+    ingredientNutrients(e.target.dataset.ndbno)
+  }
+})
+
 document.querySelector('#searchBtn').addEventListener('click', e => {
   e.preventDefault()
   if (standardButton.checked === false && brandedButton.checked === false) {
@@ -41,10 +47,30 @@ const databaseSelect = _ => {
 
 const printIngredientInfo = _ => {
   console.log(ingredientInfo)
+  let nutritionInfoTable = document.createElement('div')
+  nutritionInfoTable.id = 'nutritionInfoTable'
+  nutritionInfoTable.innerHTML =
+    `
+  <h5>${ingredientInfo.name} Nutrients</h5>
+  <ul>
+  <li>Calories:</li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  </ul>
+  `
+  document.querySelector('#nutritionFacts').append(nutritionInfoTable)
 }
 
 const nutritionTable = r => {
   let nutrientReport = r.report.food.nutrients
+  console.log(nutrientReport)
 
   for (let i = 0; i < nutrientReport.length; i++) {
     let nutrientID = nutrientReport[i].nutrient_id
@@ -77,16 +103,16 @@ const nutritionTable = r => {
 
 const ingredientNutrients = NDBno => {
   document.querySelector('#ingredients').innerHTML = ''
+  document.querySelector('#foodOptions').style.display = 'none'
   document.querySelector('.ingredientOption')
 
   fetch(`https://api.nal.usda.gov/ndb/reports/?ndbno=${NDBno}&type=f&format=json&api_key=${apiUSDA}&measureby=m`)
     .then(r => r.json())
     .then(r => {
-      // console.log(r.report.food)
       if (r.report.food.cn === '') {
-        console.log(`${r.report.food.name} name`)
+        ingredientInfo.name = r.report.food.name
       } else {
-        console.log(`${r.report.food.cn} cn`)
+        ingredientInfo.name = r.report.food.cn
       }
       nutritionTable(r)
     })
@@ -94,6 +120,7 @@ const ingredientNutrients = NDBno => {
 
 const searchItems = _ => {
   searchItem = document.querySelector('#searchItem').value
+  document.querySelector('#ingredients').style.display = 'inline'
   document.querySelector('#ingredients').innerHTML = ''
 
   fetch(`https://api.nal.usda.gov/ndb/search/?format=json&q=${searchItem}&sort=r&ds=${database}&max=10&offset=${offset}&api_key=${apiUSDA}`)
@@ -113,9 +140,3 @@ const searchItems = _ => {
       })
     })
 }
-
-document.addEventListener('click', e => {
-  if (e.target.className === 'ingredientOption') {
-    ingredientNutrients(e.target.dataset.ndbno)
-  }
-})
