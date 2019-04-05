e88b7289544085b3979347e711f4c5d4e0ee5564
Pandapoc <45634681+Pandapoc@users.noreply.github.com>
2019-04-04 20:33:29 -0700

    HeLp :(

--- JS/youtube.js
+++ JS/youtube.js
@@ -1,17 +1,18 @@
-// // Youtube API
-// let apiYoutube = 'AIzaSyD9YNmSrcL7KpEt8gD9eSdDiSMpoY1CQdc'
-// // let searchRecipes ='applepie'
-// fetch(`https://www.googleapis.com/playlists?part=snippet&channelId=UC_x5XG10V2P6uZZ5FSM9Ttw&key=${apiYoutube}`)
-//  .then(r => r.json())
-//  .then(r => {
-//      console.log(r)
-//  })
-//  .catch(e => console.error(e))
- 
-document.addEventListener('click', e =>{
-    e.preventDefault()
-    if(e.target.id === 'searchRecipes'){
-        let recipes = document.querySelector("#Recipes").value
-        document.querySelector("#ytplayer").setAttribute("src", `https://www.youtube.com/embed?listType=search&list=${recipes}`)}
-    })
-    
+
+
+// let vidSearch = 'garlic chicken'
+
+// fetch(`https://www.googleapis.com/youtube/v3/search?part=id&q=${vidSearch}&type=video&key=AIzaSyBDmq6-SlY6LWmYDPunDUoOxU8fR07rDpA`)
+// .then(r => r.json())
+// .then(r => {
+// console.log(r.items[2].id.videoId)
+
+//     vidId = r.items[2].id.videoId
+
+//     let vidElem = document.createElement('div')
+//     vidElem.innerHTML= `
+//     <iframe width="320" height="160" src="https://www.youtube.com/embed/${vidId}" frameborder="0" allowfullscreen></iframe>
+//     `
+
+//     document.querySelector('#recipeLink').append(vidElem)
+// })
