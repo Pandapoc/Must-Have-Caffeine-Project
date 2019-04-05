6829c0b99621fc0662238d8850464e83d543998a
Nuo <nuot@uci.edu>
2019-04-04 21:14:42 -0700

    add login

--- JS/login.js
+++ JS/login.js
@@ -1,26 +1,46 @@
 // Initialize Firebase
+// FirebaseUI config.
+
 var config = {
     apiKey: "AIzaSyCPt5qX-k4l600vUyHwS1toWW5lhn2QvH4",
     authDomain: "must-have-caffine-project.firebaseapp.com",
     databaseURL: "https://must-have-caffine-project.firebaseio.com",
     projectId: "must-have-caffine-project",
     storageBucket: "must-have-caffine-project.appspot.com",
-    messagingSenderId: "588514633351",
+    messagingSenderId: "588514633351"
 }
 firebase.initializeApp(config)
+
 
 let db = firebase.firestore()
 let storage = firebase.storage()
 let auth = firebase.auth
 
 const uiConfig = {
-    signInSuccessUrl : 'homepage1.html',
+    signInSuccessURL: `homepage1.html`,
     signInOptions: [
-        auth.GoogleAuthProvider.PROVIDER_ID
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID
     ],
-    tosUrl:''
-
+    tosUrl: `https://google.com`,
+    privacyPolicyUrl: function () {
+        window.location.assign(`https://google.com`);
+    }
 }
+
+const ui = new firebaseui.auth.AuthUI(auth())
+ui.start(`#login`, uiConfig)
+
+// auth().onAuthStateChanged(user => {
+//     if (user) {
+//         document.querySelector('#login').style.display = 'none'
+//         document.querySelector('#signOut').style.display = 'none'
+//     }
+// })
+
+// document.querySelector('#signOut').addEventListener('click', e => {
+//     auth().signOut()
+//     window.location.reload()
+// })
 
 
 // document.querySelector('#sbmt').addEventListener('click',
